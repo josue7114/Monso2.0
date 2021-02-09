53efa3569c53dc9f4d8b0369705af223ef310d5f
Josue <josue7114@gmail.com>
2021-02-08 18:02:58 -0600

    Primer avance Monso2.0

---
+++ conservas/InicioConservas.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Monso Conservas -Inicio-</title>
+        <link href="../resources/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <script src="../resources/JQuery-3.5.1.js" type="text/javascript"></script>
+        <script src="../resources/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <link href="../resources/ConservaStyles.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body class="">
+            <header>
+            <nav class="navbar navbar-expand-lg navbar-light navCon">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="../index.php"><img src="../resources/img/Monso.svg" class="logo-monso"/></a>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link " href="../conservas/InicioConservas.php">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Productos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <div class="container">
+             
+        </div>
+        <footer class="footer">
+            <div>
+                <p class="foot">Sitio web desarrollado por <a href="https://www.linkedin.com/in/josu%C3%A9-cordero-5b9747163/" target="_blank">Cordero</a></p>
+            </div>
+        </footer>
+    </body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Página de inicio -Monso-</title>
+        <link href="resources/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+        <link href="resources/styles.css" rel="stylesheet" type="text/css"/>
+        <script src="resources/JQuery-3.5.1.js" type="text/javascript"></script>
+        <script src="resources/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+    <body class="container">
+        <h1 class="titleMain">Bienvenidos a Monso</h1>
+        <div class="row containMain">
+            <div class="col-md-6">
+                <h3 class="titleMain">Monso Cosmética natural</h3>
+                <div class="col-md-6 sectionImg">
+                    <a href="cosmetica/InicioCosmetica.php"><img class="w-100 imgMain" src="resources/img/MonsoCosmetica.png"></a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <h3 class="titleMain">Monso Conservas naturales</h3>
+                <div class="col-md-6 sectionImg">
+                    <a href="conservas/InicioConservas.php"><img class="w-100 imgMain" src="resources/img/MonsoConserva.png"></a>
+                </div>
+            </div>
+        </div>
+        <footer class="footer">
+            <div>
+                <p class="foot">Sitio web desarrollado por <a href="https://www.linkedin.com/in/josu%C3%A9-cordero-5b9747163/" target="_blank">Cordero</a></p>
+            </div>
+        </footer>
+    </body>
+</html>
